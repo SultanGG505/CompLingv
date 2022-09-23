cbe083505b65b164c44531f18b047f6c1d19e751
Sultan <73115468+SultanGG505@users.noreply.github.com>
2022-09-23 08:26:08 +0300

    stopwords added

--- FirstLab.py
+++ FirstLab.py
@@ -2,11 +2,13 @@ path = "DataSet.Txt"  # Путь к файлу с датасетом
 
 from nltk import sent_tokenize  # импортирование пакета для разделения текста на предложения
 from nltk import word_tokenize  # импортирование пакета для разделения предложений на слова
-
+from nltk.corpus import stopwords
 file = open(path, "r", encoding="utf8")  # открытие файла с текстом датасета
 
 text = file.readlines()  # чтение всех строк из файла с текстом датасета
 d = 1  # переменная для нумерации предложений в тексте
+
+st_words = set(stopwords.words('russian'))
 
 # каждую строчку текста(с учётом того, что на одной строке документа может быть несколько предложений)
 # разбиваем предложения, потом выводим каждое предложение, причём выведя предложение, сразу разбиваем его на слова
@@ -18,6 +20,7 @@ for el in text:
         sent = sentences[i]
         d += 1
         words = word_tokenize(sent)
+        without_stop_words = [word for word in words if not word in st_words]
         print("Слова из этого предложения: ", sep="")
-        print(words, sep=" ")
+        print(without_stop_words, sep=" ")
         print("\n")
